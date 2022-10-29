8fb17be866d673a487af7dfd60ff761548d5b51b
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2022-10-29 16:43:53 -0700

    Create LSC 100%.cpp

---
+++ LSC 100%.cpp
@@ -0,0 +1,32 @@
+// Version 1.63
+
+
+					
+
+
+			    STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CHAR_FM_CARMOD_3_UNLCK"), -1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CHAR_FM_CARMOD_4_UNLCK"), -1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CHAR_FM_CARMOD_5_UNLCK"), -1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CHAR_FM_CARMOD_7_UNLCK"), -1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMRALLYWONDRIVE"), 1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMRALLYWONNAV"), 1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMWINSEARACE"), 1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMWINAIRRACE"), 1, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "NUMBER_TURBO_STARTS_IN_RACE"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "USJS_FOUND"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "USJS_COMPLETED"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "RACES_WON"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FM_RACES_FASTEST_LAP"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "NUMBER_SLIPSTREAMS_IN_RACE"), 100, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_WIN_CAPTURES"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_DROPOFF_CAP_PACKAGES"), 100, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_KILL_CARRIER_CAPTURE"), 100, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FINISH_HEISTS"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FINISH_HEIST_SETUP_JOB"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_NIGHTVISION_KILLS"), 100, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_WIN_LAST_TEAM_STANDINGS"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_ONLY_PLAYER_ALIVE_LTS"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MPPLY_TOTAL_RACES_WON"), 50, true);
+					STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MPPLY_TOTAL_RACES_LOST"), 43, true);
+
+					

d927eb78098685b894e095d5db02e5f2876e0855
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2022-10-29 16:48:23 -0700

    Create MaxStats.cpp

---
+++ MaxStats.cpp
@@ -0,0 +1,15 @@
+// Gta V 1.63
+
+
+
+
+
+
+                              std::string mpPrefix = local_player::get_mp_prefix();
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_FLY"), 100, true); // Fly
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_DRIV"), 100, true); // Drive
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_LUNG"), 100, true); // Lung
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_SHO"), 100, true); // Shoot
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_STAM"), 100, true); // Stamina
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_STL"), 100, true); // Sigile
+                                STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SCRIPT_INCREASE_STRN"), 100, true); // Strenght

1d437d04d2735233f2062500b05f5aaca6ed3f96
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2022-10-29 16:45:05 -0700

    Create RP Correction.cpp

---
+++ RP Correction.cpp
@@ -0,0 +1,6 @@
+// GTA V 1.63
+
+
+// Level 135
+      std::string mpPrefix = local_player::get_mp_prefix();
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CHAR_SET_RP_GIFT_ADMIN"), 2464100, true);

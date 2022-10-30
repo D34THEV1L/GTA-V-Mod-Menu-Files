fa5d51fb5b103686cbd5eae7bb44d02096bf0d38
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2022-10-29 17:42:11 -0700

    Create Ammu-Nation 100%.cpp

---
+++ Ammu-Nation 100%.cpp
@@ -0,0 +1,28 @@
+// Gta v 1.63
+
+std::string mpPrefix = local_player::get_mp_prefix();
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SAWNOFF_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "BULLPUP_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "ASLTSHTGN_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SNIPERRFL_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "HVYSNIPER_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "GRNLAUNCH_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SMG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "ASLTSMG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "ASLTRIFLE_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CRBNRIFLE_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "ADVRIFLE_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CMBTMG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "ASLTMG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "PUMP_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MOLOTOV_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "CMBTPISTOL_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "PISTOL50_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "APPISTOL_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "RPG_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MINIGUNS_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "GRENADE_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "SMKGRENADE_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "STKYBMB_ENEMY_KILLS"), 600, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MOLOTOV_ENEMY_KILLS"), 600, true);

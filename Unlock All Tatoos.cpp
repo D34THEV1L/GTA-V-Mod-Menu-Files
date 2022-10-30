2a856da41eedbac4a47df51faeaaf4834230569e
D34TH_3V1L <87221905+D34THEV1L@users.noreply.github.com>
2022-10-29 17:32:49 -0700

    Create Unlock All Tatoos.cpp

---
+++ Unlock All Tatoos.cpp
@@ -0,0 +1,23 @@
+// Gta v 1.63
+
+
+std::string mpPrefix = local_player::get_mp_prefix();
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "KILLS_PLAYERS"), 1000, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "PLAYER_HEADSHOTS"), 500, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_CAR_BOMBS_ENEMY_KILLS"), 25, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FM_DM_WINS"), 50, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_HOLD_UP_SHOPS"), 19, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_RACES_WON"), 50, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FM_DM_TOTALKILLS"), 500, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMATTGANGHQ"), true ,true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FM_TDM_MVP"), 50, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMKILLBOUNTY"), 25, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMWINEVERYGAMEMODE"), true, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMREVENGEKILLSDM"), 50, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMKILLSTREAKSDM"), true, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "AWD_FMBBETWIN"), 50000, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMMOSTKILLSSURVIVE"), true, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "LAP_DANCED_BOUGHT"), 25, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMKILL3ANDWINGTARACE"), true, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMRACEWORLDRECHOLDER"), 100, true);
+			STATS::STAT_SET_BOOL(rage::joaat(mpPrefix + "AWD_FMFULLYMODDEDCAR"), true, true);
